33313c020dae2bdd3fb7d58c7009c04d69c5bc54
Brian Green <briangreen@Brians-MacBook-Pro.local>
2023-07-03 11:11:24 -0400

    Updated Resume

--- src/Components/header/CTA.jsx
+++ src/Components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/BG-Resume.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <a href={CV} download className='btn'>Download CV</a>
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
