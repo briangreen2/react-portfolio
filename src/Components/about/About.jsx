c6bb9f97a8d8a91cb1b0bec812814fd1b4931d4f
Brian Green <briangreen@Brians-MacBook-Pro.local>
2023-07-03 10:29:25 -0400

    First commit

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/professional-pic.jpeg'
+import ME from '../../assets/Linkedin-pic.jpeg'
 import { FaAward } from 'react-icons/fa'
 import { FiUsers } from 'react-icons/fi'
 import { FaFolderOpen } from 'react-icons/fa'
